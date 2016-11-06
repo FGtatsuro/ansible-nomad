9844e01c70f63d1b302870d8e6599f0ec3fd7663
FGtatsuro <fujiistorage@gmail.com>
2016-11-07 02:08:09 +0900

    Add specs related to role variables for Nomad configs.

--- spec/nomad_spec.rb
+++ spec/nomad_spec.rb
@@ -12,3 +12,14 @@ describe file('/usr/local/bin/nomad') do
   it { should be_executable }
 end
 
+[
+  "#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/server.hcl",
+  "#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/client.hcl"
+].each do |f|
+  describe file(f) do
+    it { should be_file }
+    it { should be_readable }
+    it { should be_owned_by ENV['NOMAD_CONFIG_OWNER'] }
+    it { should be_grouped_into ENV['NOMAD_CONFIG_GROUP'] }
+  end
+end
