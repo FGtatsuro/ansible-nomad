b6911f8687cdec132deddb2ac86a92d473709a65
FGtatsuro <fujiistorage@gmail.com>
2017-02-01 16:59:12 +0900

    Bump up default version of Nomad. 0.5.0->0.5.4

--- spec/nomad_spec.rb
+++ spec/nomad_spec.rb
@@ -5,7 +5,7 @@ describe command('nomad version') do
 end
 
 describe command('nomad version'), :if => ['debian'].include?(os[:family]) do
-  its(:stdout) { should contain("Nomad v0.5.0") }
+  its(:stdout) { should contain("Nomad v0.5.4") }
 end
 
 describe file('/usr/local/bin/nomad') do
