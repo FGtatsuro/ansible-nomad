61def4ea7d1c61eeff7af9758906ee5eb729c6c1
FGtatsuro <fujiistorage@gmail.com>
2017-03-16 12:17:02 +0900

    Nomad config isn't json.

--- spec/nomad_spec.rb
+++ spec/nomad_spec.rb
@@ -51,7 +51,7 @@ describe file(ENV['NOMAD_CONFIG_REMOTE_DIR']) do
 end
 
 # Common settings
-describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
   it { should be_file }
   it { should be_readable }
   it { should be_owned_by ENV['NOMAD_OWNER'] }
@@ -62,14 +62,14 @@ describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
 end
 
 if ENV['NOMAD_ADVERTISE_CONFIG_ADDR'] then
-  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
     its(:content) { should match /advertise/ }
     its(:content) { should match /#{Regexp.escape("http = \"#{ENV['NOMAD_ADVERTISE_CONFIG_ADDR']}\"")}/ }
     its(:content) { should match /#{Regexp.escape("rpc = \"#{ENV['NOMAD_ADVERTISE_CONFIG_ADDR']}\"")}/ }
     its(:content) { should match /#{Regexp.escape("serf = \"#{ENV['NOMAD_ADVERTISE_CONFIG_ADDR']}\"")}/ }
   end
 else
-  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
     its(:content) { should_not match /advertise {/ }
     its(:content) { should_not match /http =/ }
     its(:content) { should_not match /rpc =/ }
@@ -78,53 +78,53 @@ else
 end
 
 if ENV['NOMAD_CONSUL_ADDRESS'] then
-  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
     its(:content) { should match /consul {/ }
     its(:content) { should match /#{Regexp.escape("address = \"#{ENV['NOMAD_CONSUL_ADDRESS']}\"")}/ }
   end
 else
-  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
     its(:content) { should_not match /consul {/ }
     its(:content) { should_not match /address = / }
   end
 end
 
 if ENV['NOMAD_JOIN_SERVER'] then
-  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
     its(:content) { should match /client {\n(.*\n){0,2}  #{Regexp.escape("servers = [\"#{ENV['NOMAD_JOIN_SERVER']}\"]")}/ }
   end
 else
-  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
     its(:content) { should_not match /client {\n(.*\n){0,2}  #{Regexp.escape('servers = [')}/ }
   end
 end
 
 if ENV['NOMAD_CLIENT'] then
-  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
     its(:content) { should match /client {\n(.*\n){0,2}  enabled = #{ENV['NOMAD_CLIENT']}/ }
   end
 else
-  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
     its(:content) { should_not match /client {\n(.*\n){0,2}  enabled =/ }
   end
 end
 
 if ENV['NOMAD_BOOTSTRAP_EXPECT'] then
-  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
     its(:content) { should match /server {\n(.*\n){0,2}  bootstrap_expect = #{ENV['NOMAD_BOOTSTRAP_EXPECT']}/ }
   end
 else
-  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
     its(:content) { should_not match /server {\n(.*\n){0,2}  bootstrap_expect =/ }
   end
 end
 
 if ENV['NOMAD_SERVER'] then
-  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
     its(:content) { should match /server {\n(.*\n){0,2}  enabled = #{ENV['NOMAD_SERVER']}/ }
   end
 else
-  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.json") do
+  describe file("#{ENV['NOMAD_CONFIG_REMOTE_DIR']}/nomad_common.hcl") do
     its(:content) { should_not match /server {\n(.*\n){0,2}  enabled =/ }
   end
 end
